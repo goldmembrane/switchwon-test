b05cdaf84c2ebebbee767cf86acf7cf3fb7a20e1
= <=>
2026-01-13 20:32:00 +0900

    implement login logic

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,11 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 function LogIn() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const base = import.meta.env.DEV ? "/api" : apiUrl;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`${base}/auth/login`, null, {
+        params: { email },
+      })
+      .then((response) => {
+        console.log("Login successful:", response.data);
+        localStorage.setItem("token", response.data.data.token);
+        navigate("/home");
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.error(
+            "Login failed:",
+            error.response.status,
+            error.response.data
+          );
+        } else {
+          console.error("Login failed:", error.message);
+        }
+      });
+  };
+
   return (
     <div>
       <h1>LogIn Page</h1>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={handleEmailChange}
+        value={email}
+      />
 
-      <button>LogIn</button>
+      <button onClick={handleSubmit}>LogIn</button>
     </div>
   );
 }
